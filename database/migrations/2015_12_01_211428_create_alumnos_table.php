40288cb69b5f453bd9e906d2ea5f9c3ff8d24ba5
wlarodri <wlarodri@hotmail.es>
2015-12-01 18:45:02 -0300

    crea proyecto laravel

---
+++ database/migrations/2015_12_01_211428_create_alumnos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlumnosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('alumnos', function(Blueprint $table)
+		{
+			$table->string('nombre',255);
+			$table->string('rut',11);
+			$table->increments('id');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('alumnos');
+	}
+
+}
