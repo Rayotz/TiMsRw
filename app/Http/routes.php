8c39675097fe4dd55644667eee743d4519f36f7e
Rayotz <sildouster@gmail.com>
2015-12-23 07:41:47 -0300

    Arreglo de rutas de daniel (NO LAS ORDENEN!)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,30 +9,16 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-//Rutas controladores de propuestas de proyecto del profesor
 Route::controller("/Propuesta/profesor", 'PropuestaProfesorController');
-Route::get("/Propuesta/profesor", 'PropuestaProfesorController@getValidacion');
-Route::post("/Propuesta/profesor", 'PropuestaProfesorController@postValidacion');
-
-//Rutas controladores de propuestas de proyecto del alumno
-Route::controller("/Propuesta/alumno", "PropuestaAlumnoController");
-Route::get("/Propuesta/alumno", 'PropuestaAlumnoController@getValidacion2');
-Route::post("/Propuesta/alumno", 'PropuestaAlumnoController@postValidacion2');
-
-
 Route::controller('/registrarProfesor','controladorRegistroProfesor');
 Route::controller('/registrarAlumno','controlladorRegistroAlumno');
 Route::controller('/','controladorLogin');
-
 //Route::get('/', 'WelcomeController@index');
 //Route::get('home', 'HomeController@index');
-
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
-
 //rutas tablas
 Route::resource('alumno','AlumnosController');
 Route::resource('usuario','UsuariosController');
@@ -42,10 +27,12 @@ Route::resource('grupo','GruposController');
 Route::resource('profesor','ProfesorsController');
 Route::resource('tema','TemasController');
 Route::resource('tipo','TiposController');
-
-
-
-
+//Rutas controladores de propuestas de proyecto del profesor
+Route::controller("/Propuesta/profesor", 'PropuestaProfesorController');
+Route::get("/Propuesta/profesor", 'PropuestaProfesorController@getValidacion');
+Route::post("/Propuesta/profesor", 'PropuestaProfesorController@postValidacion');
+//Rutas controladores de propuestas de proyecto del alumno
+//Route::controller("/Propuesta/alumno", "PropuestaAlumnoController");
 
 
 
