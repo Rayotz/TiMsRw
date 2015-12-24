21fd9ce85a30d6b02d4bde50c847d4913536a914
Emilio Yañez <emyanezmeneses@gmail.com>
2015-12-23 23:37:18 -0300

    cambio en controller, y creacion  de request de grupo

--- app/Grupo.php
+++ app/Grupo.php
@@ -4,6 +4,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Grupo extends Model {
 
-    protected $fillable=['gru_numero','gru_tamano_grupo'];
+    protected $table = '';
+    protected $fillable =[
+
+        'gru_numero',
+        'gru_tamano_gru',
+        'reate_at',
+        'updated_at',
+    ];
 
 }
